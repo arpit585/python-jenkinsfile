a7878ae89bad8879b0e722a5e3a3f66c7676cbd2
arpit <arpittiwari585@gmail.com>
2022-09-19 17:17:33 +0530

    first commit

---
+++ jenkinsfile.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'f8352143-8cd3-4f0f-806e-ba5cd088da48', url: 'https://github.com/arpit585/learning-git.git']]])
+            }
+        }
+        stage('Build'){
+            steps{
+                git credentialsId: 'f8352143-8cd3-4f0f-806e-ba5cd088da48', url: 'https://github.com/arpit585/learning-git.git'
+                bat 'python test3.py'
+            }
+            }
+        stage('Test'){
+            steps{
+                echo 'job has been tested'
+            }
+        }
+            
+        }
+
+
+}
